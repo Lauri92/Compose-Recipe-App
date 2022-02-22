2ba2ec45f6f98772287ef5d4a9fefd08d14937a0
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-02-22 14:29:39 +0200

    Added favorites screen content to resemble the list of that in search screen

--- app/src/main/java/fi/lauriari/recipe_app/screens/favorites/FavoritesContent.kt
+++ app/src/main/java/fi/lauriari/recipe_app/screens/favorites/FavoritesContent.kt
@@ -1,24 +1,79 @@
 package fi.lauriari.recipe_app.screens.favorites
 
-import androidx.compose.foundation.layout.Column
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import fi.lauriari.recipe_app.data.entities.FavoriteRecipe
 import fi.lauriari.recipe_app.data.entities.RecipeWithIngredientLines
+import fi.lauriari.recipe_app.screens.search.SingleRecipe
 import fi.lauriari.recipe_app.viewmodels.MainViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FavoritesContent(
     mainViewModel: MainViewModel,
     allFavoriteRecipes: State<List<RecipeWithIngredientLines>>
 ) {
 
-    Column() {
-        allFavoriteRecipes.value.forEach { combination ->
-            Text(combination.favoriteRecipe.label)
-            combination.ingredientLines.forEach { ingredientLine ->
-                Text(ingredientLine.description)
+    val context = LocalContext.current
+
+    val listState = rememberLazyListState()
+
+    LazyVerticalGrid(
+        state = listState,
+        cells = GridCells.Adaptive(minSize = 160.dp),
+        contentPadding = PaddingValues(
+            start = 21.dp,
+            top = 16.dp,
+            end = 12.dp,
+            bottom = 16.dp
+        ),
+    ) {
+        items(
+            items = allFavoriteRecipes.value,
+        ) { favorite ->
+            Column(
+                modifier = Modifier
+                    .clickable {
+
+                    }
+                    .padding(top = 10.dp, bottom = 10.dp)
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = rememberImagePainter(
+                        data = favorite.favoriteRecipe.imageUrl,
+                        builder = {
+                            //placeholder()
+                            //error()
+                            crossfade(500)
+                            transformations(
+                                CircleCropTransformation()
+                            )
+                        }
+                    ),
+                    contentDescription = "Image of a recipe",
+                    modifier = Modifier.size(150.dp)
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(all = 2.dp),
+                    text = favorite.favoriteRecipe.label
+                )
             }
         }
     }
